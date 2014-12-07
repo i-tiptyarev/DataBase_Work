7cf110c0452cf3d281fc8b2ed0ec1c589ec5d527
i-tiptyarev <t.vanek13@mail.ru>
2014-12-07 18:12:37 +0300

    Design

---
+++ Hostels_SettlingProg/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Hostels_SettlingProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.операцииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьСтудентаВОбщежитиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выселитьСтудентаИзОбщежитияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.показатьВсехСтудентовВОбщежитияхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.общежитияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьНовоеОбщежитиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.снестиОбщежитиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.показатьИнформациюООбщежитииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.комнатыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.построитьКомнатуВОбщежитииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.убратьКомнатуИзОбщежитияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.показатьСтатусКомнатыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.операцииToolStripMenuItem,
+            this.общежитияToolStripMenuItem,
+            this.комнатыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // операцииToolStripMenuItem
+            // 
+            this.операцииToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьСтудентаВОбщежитиеToolStripMenuItem,
+            this.выселитьСтудентаИзОбщежитияToolStripMenuItem,
+            this.показатьВсехСтудентовВОбщежитияхToolStripMenuItem});
+            this.операцииToolStripMenuItem.Name = "операцииToolStripMenuItem";
+            this.операцииToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.операцииToolStripMenuItem.Text = "Студенты";
+            // 
+            // добавитьСтудентаВОбщежитиеToolStripMenuItem
+            // 
+            this.добавитьСтудентаВОбщежитиеToolStripMenuItem.Name = "добавитьСтудентаВОбщежитиеToolStripMenuItem";
+            this.добавитьСтудентаВОбщежитиеToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.добавитьСтудентаВОбщежитиеToolStripMenuItem.Text = "Заселить студента в общежитие";
+            // 
+            // выселитьСтудентаИзОбщежитияToolStripMenuItem
+            // 
+            this.выселитьСтудентаИзОбщежитияToolStripMenuItem.Name = "выселитьСтудентаИзОбщежитияToolStripMenuItem";
+            this.выселитьСтудентаИзОбщежитияToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.выселитьСтудентаИзОбщежитияToolStripMenuItem.Text = "Выселить студента из общежития";
+            // 
+            // показатьВсехСтудентовВОбщежитияхToolStripMenuItem
+            // 
+            this.показатьВсехСтудентовВОбщежитияхToolStripMenuItem.Name = "показатьВсехСтудентовВОбщежитияхToolStripMenuItem";
+            this.показатьВсехСтудентовВОбщежитияхToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.показатьВсехСтудентовВОбщежитияхToolStripMenuItem.Text = "Показать информацию о студентах";
+            // 
+            // общежитияToolStripMenuItem
+            // 
+            this.общежитияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьНовоеОбщежитиеToolStripMenuItem,
+            this.снестиОбщежитиеToolStripMenuItem,
+            this.показатьИнформациюООбщежитииToolStripMenuItem});
+            this.общежитияToolStripMenuItem.Name = "общежитияToolStripMenuItem";
+            this.общежитияToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.общежитияToolStripMenuItem.Text = "Общежития";
+            // 
+            // добавитьНовоеОбщежитиеToolStripMenuItem
+            // 
+            this.добавитьНовоеОбщежитиеToolStripMenuItem.Name = "добавитьНовоеОбщежитиеToolStripMenuItem";
+            this.добавитьНовоеОбщежитиеToolStripMenuItem.Size = new System.Drawing.Size(282, 22);
+            this.добавитьНовоеОбщежитиеToolStripMenuItem.Text = "Построить новое общежитие";
+            // 
+            // снестиОбщежитиеToolStripMenuItem
+            // 
+            this.снестиОбщежитиеToolStripMenuItem.Name = "снестиОбщежитиеToolStripMenuItem";
+            this.снестиОбщежитиеToolStripMenuItem.Size = new System.Drawing.Size(282, 22);
+            this.снестиОбщежитиеToolStripMenuItem.Text = "Снести общежитие";
+            // 
+            // показатьИнформациюООбщежитииToolStripMenuItem
+            // 
+            this.показатьИнформациюООбщежитииToolStripMenuItem.Name = "показатьИнформациюООбщежитииToolStripMenuItem";
+            this.показатьИнформациюООбщежитииToolStripMenuItem.Size = new System.Drawing.Size(282, 22);
+            this.показатьИнформациюООбщежитииToolStripMenuItem.Text = "Показать информацию о общежитии";
+            // 
+            // комнатыToolStripMenuItem
+            // 
+            this.комнатыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.построитьКомнатуВОбщежитииToolStripMenuItem,
+            this.убратьКомнатуИзОбщежитияToolStripMenuItem,
+            this.показатьСтатусКомнатыToolStripMenuItem});
+            this.комнатыToolStripMenuItem.Name = "комнатыToolStripMenuItem";
+            this.комнатыToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.комнатыToolStripMenuItem.Text = "Комнаты";
+            // 
+            // построитьКомнатуВОбщежитииToolStripMenuItem
+            // 
+            this.построитьКомнатуВОбщежитииToolStripMenuItem.Name = "построитьКомнатуВОбщежитииToolStripMenuItem";
+            this.построитьКомнатуВОбщежитииToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.построитьКомнатуВОбщежитииToolStripMenuItem.Text = "Построить комнату в общежитии";
+            // 
+            // убратьКомнатуИзОбщежитияToolStripMenuItem
+            // 
+            this.убратьКомнатуИзОбщежитияToolStripMenuItem.Name = "убратьКомнатуИзОбщежитияToolStripMenuItem";
+            this.убратьКомнатуИзОбщежитияToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.убратьКомнатуИзОбщежитияToolStripMenuItem.Text = "Убрать комнату из общежития";
+            // 
+            // показатьСтатусКомнатыToolStripMenuItem
+            // 
+            this.показатьСтатусКомнатыToolStripMenuItem.Name = "показатьСтатусКомнатыToolStripMenuItem";
+            this.показатьСтатусКомнатыToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
+            this.показатьСтатусКомнатыToolStripMenuItem.Text = "Показать статус комнаты";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Общежития КАИ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 202);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(15, 278);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(298, 143);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 150);
+            this.label2.TabIndex = 0;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem операцииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьСтудентаВОбщежитиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выселитьСтудентаИзОбщежитияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem показатьВсехСтудентовВОбщежитияхToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem общежитияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьНовоеОбщежитиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem снестиОбщежитиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem показатьИнформациюООбщежитииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem комнатыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem построитьКомнатуВОбщежитииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem убратьКомнатуИзОбщежитияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem показатьСтатусКомнатыToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hostels_SettlingProg/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hostels_SettlingProg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработано студентами группы 4434 Типтяревым И., Рязановым П., Чирковым Ю.");
+        }
+    }
+}
